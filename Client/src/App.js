229684b8d9181662f197ba38e513380283cddda0
mauro mariscal <mariscalmauro0608@gmail.com>
2025-04-03 20:13:29 -0700

    front-boilercode

---
+++ Client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Pawsome Vibes 🐶</h1>
+      <p>Welcome to our app!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ Client/src/services/mockData.js
@@ -0,0 +1,12 @@
+export const mockAffirmations = [
+    { id: 1, text: "You are stronger than you think! 💪" },
+    { id: 2, text: "Keep pushing forward! 🚀" },
+    { id: 3, text: "You are enough, just as you are. 🌟" },
+  ];
+  
+  export const mockDogImages = [
+    "https://placedog.net/400/300",
+    "https://placedog.net/500/400",
+    "https://placedog.net/600/500",
+  ];
+  
